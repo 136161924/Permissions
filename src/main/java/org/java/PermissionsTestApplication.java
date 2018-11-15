0ee679c7a375da1c20c71f4d3ec05f1e2c50f612
136161924 <136161924@qq.com>
2018-11-15 21:54:34 +0800

    测试提交第一次

---
+++ src/main/java/org/java/PermissionsTestApplication.java
@@ -0,0 +1,12 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PermissionsTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PermissionsTestApplication.class, args);
+    }
+}
